65248f887f3a57f1d0936075b3aa14f8adaa4175
ledapires <ledanashira@yahoo.com.br>
2025-07-25 23:19:16 -0300

    Criei um arquivo de configuração e apliquei variáveis de ambiente

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,10 +1,11 @@
 import http from 'k6/http';
+import { pegarBaseURL } from '../utils/variaveis.js';
+
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export function obterToken(){
-    const url = 'http://localhost:3000/login';
+    const url = pegarBaseURL() + '/login';
     
-        console.log(postLogin)
         const payload = JSON.stringify(postLogin);
     
         const params = {
